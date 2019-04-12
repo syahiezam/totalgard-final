600fe924b23fc5fbdb277de20f8e050ec178068b
Syahiezam <m.syhzm@gmail.com>
2019-04-12 22:36:30 +0800

    beta1

---
+++ test.php
@@ -0,0 +1,84 @@
+<div class="col-sm-12  text-center">
+  <h1>SUSTAINABILITY AT TOTALGARD</h1>
+  <div class="container-fluid text-center" style="width:40%">
+    <p>At Totalgard, sustainability is one of the upmost important thing that
+    we persue. We make use of materials which are unrecycable to produce products
+  which are beneficial for people and the planet.</p>
+</div>
+
+
+<div class="container">
+  <div class="row">
+
+    <div class="col-sm">
+
+      <div class="container-fluid text-center">
+        <img class="gridicon" src="images/igif_1.gif">
+        <p>Customer Focused</p>
+        <div class="text-justify">
+        <p>We are seeking to build a long term and effective customer base.
+We strive to continually meet and surpass customers expectation
+as well as the industry’s standard.</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-sm">
+      <div class="container-fluid text-center">
+        <img class="gridicon" src="images/igif_2.gif">
+        <p>Safety</p>
+        <div class="text-justify">
+        <p>Inside our manufacturing process, we strictly abide to ISO9001:2015 certification to
+ensure that our quality is consistent. All our products are MS certified.</p>
+      </div>
+      </div>
+    </div>
+
+    <div class="col-sm">
+      <div class="container-fluid text-center">
+        <img class="gridicon" src="images/igif_3.gif">
+        <p>Measure Performance</p>
+        <div class="text-justify">
+          <p>Effectiveness is another key element that we strive for as a competitive advantage.
+We believe in embracing a well-managed business with good developments.</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-sm">
+      <div class="container-fluid text-center">
+        <img class="gridicon" src="images/igif_4.gif">
+        <p>Quality Driven</p>
+        <div class="text-justify">
+        <p>We have attained merits from our customers for delivering high
+quality products and our aim is to provide maximum quality from
+the planning stage to the finish products.
+      </p>
+      </div>
+      </div>
+    </div>
+
+  </div>
+</div>
+</div>
+
+      <div class="container-fluid">
+        <div class="d-flex ">
+          <div class="customer">
+            <img src="images/icontall/bmtrade.png"/>
+          </div>
+          <div class="customer">
+            <img src="images/icontall/ukas.png"/>
+          </div>
+          <div class="customer">
+            <img src="images/icontall/ukas.png"/>
+          </div>
+          <div class="customer">
+            <img src="images/icontall/iso.png"/>
+          </div>
+          <div class="customer">
+            <img src="images/icontall/cancer.jpg"/>
+          </div>
+
+        </div>
+      </div>
